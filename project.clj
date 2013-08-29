94d88f11154276c892b183a5742d55a6851a5273
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2013-08-29 04:25:00 +0300

    scm

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+         :url "https://github.com/danielsz/reloaded2"})
