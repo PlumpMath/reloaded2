45004965ca8013c63b7f88321152b352d3813838
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2013-09-07 16:42:42 +0300

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded2/lein-template "0.1.3"
+(defproject reloaded2/lein-template "0.1.4"
   :description "Created with lein-create-template"
   :url "https://github.com/danielsz/reloaded2"
   :license {:name "Eclipse Public License"
