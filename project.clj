052074fe92dd6bdf7c6602564db9b6fc03172fac
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-03-09 00:18:09 +0200

    liberator 0.11.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded2/lein-template "0.2.4"
+(defproject reloaded2/lein-template "0.2.5"
   :description "Created with lein-create-template"
   :url "https://github.com/danielsz/reloaded2"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/reloaded2/project.clj
+++ src/leiningen/new/reloaded2/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [liberator "0.10.0"]
+                 [liberator "0.11.0"]
                  [ring "1.2.1"]
                  [compojure "1.1.6"]
                  [environ "0.4.0"]]
